626908f065148fff8db9ec81da39223ec1994aff
Carson Bolter <208@holbertonschool.com>
2017-05-11 11:15:19 -0700

    Changed file permissions

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-    char c;
-    int i;
-    long l;
-    long long ll;;
-    float f;
+        char c;
+        int i;
+        long l;
+        long long ll;;
+        float f;
 
-    printf("Size of a char: %lu bytes\n", sizeof(c));
-    printf("Size of an int: %lu bytes\n", sizeof(i));
-    printf("Size of a long int: %lu bytes\n", sizeof(l));
-    printf("Size of a long long int: %lu bytes\n", sizeof(ll));
-    printf("Size of a float is: %lu bytes\n", sizeof(f));
+        printf("Size of a char: %lu bytes\n", sizeof(c));
+        printf("Size of an int: %lu bytes\n", sizeof(i));
+        printf("Size of a long int: %lu bytes\n", sizeof(l));
+        printf("Size of a long long int: %lu bytes\n", sizeof(ll));
+        printf("Size of a float is: %lu bytes\n", sizeof(f));
 
-    return (0);
+        return (0);
 }
