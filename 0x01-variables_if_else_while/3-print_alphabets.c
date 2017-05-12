f00db282f6e9ff0d1352d64ecd8304572ecc493a
Carson Bolter <208@holbertonschool.com>
2017-05-12 21:24:14 +0000

    Appended errors

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -  Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alph = 'a';
+	char cap_alph = 'A';
+
+	while (alph <= 'z')
+	{
+	       putchar(alph);
+	       alph++;
+	}
+	while (cap_alph <= 'Z')
+	{
+	       putchar(cap_alph);
+	       cap_alph++;
+	}
+	putchar('\n');
+	return (0);
+}
