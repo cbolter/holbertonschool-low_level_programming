bff12eccec58aacf33a49ec49bdd15424e3e9a33
Carson Bolter <208@holbertonschool.com>
2017-05-12 19:22:28 +0000

    Edited 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of a char: %d byte(s)\n", sizeof(c));
 printf("Size of an int: %d byte(s)\n", sizeof(i));
 printf("Size of a long int: %d byte(s)\n", sizeof(l));
 printf("Size of a long long int: %d byte(s)\n", sizeof(li));
-printf("Size of a float is: %d byte(s)\n", sizeof(f));
+printf("Size of a float: %d byte(s)\n", sizeof(f));
 return (0);
 }
