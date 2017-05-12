35255f7766b8af8a280826dfcc14c936831e649b
Carson Bolter <208@holbertonschool.com>
2017-05-12 21:43:48 +0000

    No tabs or spaces

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,19 +7,19 @@
  */
 int main(void)
 {
-	char alph = 'a';
-	char cap_alph = 'A';
+char alph = 'a';
+char cap_alph = 'A';
 
-	while (alph <= 'z')
-	{
-	     putchar(alph);
-	     alph++;
-	}
-	while (cap_alph <= 'Z')
-	{
-	     putchar(cap_alph);
-	     cap_alph++;
-	}
-	putchar('\n');
-	return (0);
+while (alph <= 'z')
+{
+putchar(alph);
+alph++;
+}
+while (cap_alph <= 'Z')
+{
+putchar(cap_alph);
+cap_alph++;
+}
+putchar('\n');
+return (0);
 }
