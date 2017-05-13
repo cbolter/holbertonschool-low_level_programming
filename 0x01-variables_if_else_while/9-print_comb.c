f346f51ffadddd2f8a8b9b6975e841313fa5a845
Carson Bolter <208@holbertonschool.com>
2017-05-13 00:13:11 +0000

    Added project file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	num = '0';
+
+	while (num <= '9')
+	{
+		putchar(num);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(32);
+		}
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
