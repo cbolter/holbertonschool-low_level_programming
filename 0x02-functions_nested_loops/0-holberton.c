9fdd8efa26e79caab612e4a22219b16b4c6b7dd2
Carson Bolter <208@holbertonschool.com>
2017-05-19 19:15:16 +0000

    Added project files

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "_putchar.c"
+/**
+ * main - entry point
+ *
+ * Uses _putchar to print 'Holberton'
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar(10);
+	return (0);
+}
