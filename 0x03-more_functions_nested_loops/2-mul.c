8ebda1040503c723ef069d9f0dd4925bb31a5c0e
Carson Bolter <208@holbertonschool.com>
2017-05-22 18:28:06 +0000

    Added 2-mul.c and updated holberton.h

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - Multiplies 2 integers
+ * @a: First integer to be multiplied
+ * @b: Second integer to be multiplied
+ * Return: 0 if not digit, 1 if digit
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = (a * b);
+
+	return (result);
+}
