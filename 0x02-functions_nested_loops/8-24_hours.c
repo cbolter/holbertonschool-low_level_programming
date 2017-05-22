c3e1f2237d3a4886c9e3e2c412e9347b23a745b0
Carson Bolter <208@holbertonschool.com>
2017-05-22 02:37:26 +0000

    Finished

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * jack_bauer - prints all possible times in the 24 hour time format.
+ * @i: Value passed to function
+ * @one_s: Ones place for seconds
+ * @ten_m: Tens place for minutes
+ * @one_h: Ones place for hours
+ * @ten_h: Tens place for hours
+ * Return: s
+ *
+ */
+void jack_bauer(void)
+{
+	int ten_h, one_h, ten_s, one_s;
+
+	for (ten_h = 0; ten_h < 3; ten_h++)
+	{
+		for (one_h = 0; one_h < 10; one_h++)
+		{
+			if (ten_h == 2 && one_h == 4)
+			{
+				break;
+			}
+
+			for (ten_s = 0; ten_s < 6; ten_s++)
+			{
+				for (one_s = 0; one_s < 10; one_s++)
+				{
+					_putchar(ten_h + '0');
+					_putchar(one_h + '0');
+					_putchar(':');
+					_putchar(ten_s + '0');
+					_putchar(one_s + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
