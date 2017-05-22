3ca2fe2b2d9af51ea98c3654fea1ee15d0702839
Carson Bolter <208@holbertonschool.com>
2017-05-22 03:01:16 +0000

    Committed finished 10-add.c and updated holberton.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: return a + b
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
